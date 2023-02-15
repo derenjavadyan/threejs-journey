490ee553adcf3fd4bef23dbcaaeb0da041db3d9e
derenjavadyan <98952880+derenjavadyan@users.noreply.github.com>
2023-02-15 16:38:41 +0400

    9 completed

--- src/app/three-js/three-js.component.ts
+++ src/app/three-js/three-js.component.ts
@@ -17,8 +17,11 @@ export class ThreeJsComponent implements AfterViewInit {
     const scene = new THREE.Scene();
 
     //Red cube
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+      wireframe: true,
+    });
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
