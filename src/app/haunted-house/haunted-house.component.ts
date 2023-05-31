635e07a08b0a44933ebb9f058cac4c868612e15f
derenjavadyan <98952880+derenjavadyan@users.noreply.github.com>
2023-05-31 21:08:17 +0400

    fog

--- src/app/haunted-house/haunted-house.component.ts
+++ src/app/haunted-house/haunted-house.component.ts
@@ -25,6 +25,10 @@ export class HauntedHouseComponent implements AfterViewInit {
     // Scene
     const scene = new THREE.Scene();
 
+    //Fog
+    const fog = new THREE.Fog('#262837', 1, 15);
+    scene.fog = fog;
+
     /**
      * Textures
      */
@@ -64,6 +68,46 @@ export class HauntedHouseComponent implements AfterViewInit {
     house.add(door);
 
     //Bushes
+    const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+    const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' });
+
+    const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush1.scale.set(0.5, 0.5, 0.5);
+    bush1.position.set(0.8, 0.2, 2.2);
+
+    const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush2.scale.set(0.2, 0.2, 0.2);
+    bush2.position.set(1.4, 0.1, 2.1);
+
+    const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush3.scale.set(0.4, 0.4, 0.4);
+    bush3.position.set(-0.8, 0.1, 2.2);
+
+    const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+    bush4.scale.set(0.15, 0.15, 0.15);
+    bush4.position.set(-1, 0.05, 2.6);
+
+    house.add(bush1, bush2, bush3, bush4);
+
+    //Graves
+    const graves = new THREE.Group();
+    scene.add(graves);
+
+    const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+    const graveMaterial = new THREE.MeshStandardMaterial({ color: '#' });
+
+    for (let i = 0; i < 50; i++) {
+      const angle = Math.random() * Math.PI * 2;
+      const radius = 3 + Math.random() * 6;
+      const x = Math.sin(angle) * radius;
+      const z = Math.cos(angle) * radius;
+
+      const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+      grave.position.set(x, 0.3, z);
+      grave.rotation.y = (Math.random() - 0.5) * 0.4;
+      grave.rotation.z = (Math.random() - 0.5) * 0.4;
+      graves.add(grave);
+    }
 
     // Floor
     const floor = new THREE.Mesh(
@@ -78,18 +122,23 @@ export class HauntedHouseComponent implements AfterViewInit {
      * Lights
      */
     // Ambient light
-    const ambientLight = new THREE.AmbientLight('#ffffff', 0.5);
+    const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12);
     gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
     scene.add(ambientLight);
 
     // Directional light
-    const moonLight = new THREE.DirectionalLight('#ffffff', 0.5);
+    const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.12);
     moonLight.position.set(4, 5, -2);
     gui.add(moonLight, 'intensity').min(0).max(1).step(0.001);
     gui.add(moonLight.position, 'x').min(-5).max(5).step(0.001);
     gui.add(moonLight.position, 'y').min(-5).max(5).step(0.001);
     gui.add(moonLight.position, 'z').min(-5).max(5).step(0.001);
     scene.add(moonLight);
+
+    //Door light
+    const doorLight = new THREE.PointLight('#ff7d46', 1, 7);
+    doorLight.position.set(0, 2.2, 2.7);
+    house.add(doorLight);
 
     window.addEventListener('resize', () => {
       // Update sizes
@@ -103,6 +152,7 @@ export class HauntedHouseComponent implements AfterViewInit {
       // Update renderer
       renderer.setSize(this.sizes.width, this.sizes.height);
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+      renderer.setClearColor('#262837');
     });
 
     /**
@@ -132,6 +182,7 @@ export class HauntedHouseComponent implements AfterViewInit {
     });
     renderer.setSize(this.sizes.width, this.sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.setClearColor('#262837');
 
     /**
      * Animate
