92f166e2aedcf4531b13f602fae157bbb73ef154
derenjavadyan <98952880+derenjavadyan@users.noreply.github.com>
2023-06-06 20:13:28 +0400

    particles

--- src/app/particles/particles.component.ts
+++ src/app/particles/particles.component.ts
@@ -34,7 +34,17 @@ export class ParticlesComponent implements AfterViewInit {
      * Particles
      */
     //Geometry
-    const particleGeometry = new THREE.SphereGeometry(1, 32, 32);
+    const particleGeometry = new THREE.BufferGeometry();
+    const count = 5000;
+
+    const positions = new Float32Array(count * 3);
+    for (let i = 0; i < count * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 10;
+    }
+    particleGeometry.setAttribute(
+      'position',
+      new THREE.BufferAttribute(positions, 3)
+    );
 
     //Material
     const particleMaterial = new THREE.PointsMaterial({
