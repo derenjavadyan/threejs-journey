cf35f10136a0fe08b6ee1bd7df408e37a2f258ce
derenik-javadyan-t <derenik.javadyan1@tumo.org>
2023-06-23 14:28:36 +0400

    gradient texture not started

--- src/app/scroll/scroll.component.ts
+++ src/app/scroll/scroll.component.ts
@@ -22,19 +22,39 @@ export class ScrollComponent implements AfterViewInit {
     const parameters = {
       materialColor: '#ffeded',
     };
-    gui.addColor(parameters, 'materialColor');
+    gui.addColor(parameters, 'materialColor').onChange(() => {
+      material.color.set(parameters.materialColor);
+    });
 
     // Scene
     const scene = new THREE.Scene();
 
     /**
-     * Test cube
+     * Objects
      */
-    const cube = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({ color: '#ff0000' })
+    //Material
+    const material = new THREE.MeshToonMaterial({
+      color: parameters.materialColor,
+    });
+
+    //Meshes
+    const mesh1 = new THREE.Mesh(
+      new THREE.TorusGeometry(1, 0.4, 16, 60),
+      material
     );
-    scene.add(cube);
+    const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 3, 32), material);
+    const mesh3 = new THREE.Mesh(
+      new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+      material
+    );
+    scene.add(mesh1, mesh2, mesh3);
+
+    /**
+     * Lights
+     */
+    const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+    directionalLight.position.set(1, 1, 0);
+    scene.add(directionalLight);
 
     /**
      * Sizes
